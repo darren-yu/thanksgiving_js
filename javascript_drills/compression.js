5ce50409919f85e5b9be2feec73dbcd12aac5efb
darren-yu <darren.d.yu@gmail.com>
2014-11-30 23:56:06 -0800

    Update compression.js

--- javascript_drills/compression.js
+++ javascript_drills/compression.js
@@ -21,21 +21,3 @@ console.log("aaabda");
 console.log(compress("aaabda"));
 
 
-// var compress = function(string) {
-//     newString = ""
-//     count = 1
-
-//     for (var i = 0; i <= string.length; i++) {
-//         if ( string.charAt(i) != string.charAt(i+1) ) {
-//             newString += count + string.charAt(i) + " "
-//             count = 1
-//         }
-//         else{
-//             count++
-//         }
-//     }
-//     return newString
-// }
-
-// console.log("AAASSSDDDDRDDSASSDDDSSSAD")
-// console.log(compress("AAASSSDDDDRDDSASSDDDSSSAD"))
